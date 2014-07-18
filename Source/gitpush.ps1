eeb0689dce311ceeafceac8261cbed89ec7cc459
Omar AL Zabir <omaralzabir@gmail.com>
2014-07-18 17:26:56 +0100

    New version

--- Source/gitpush.ps1
+++ Source/gitpush.ps1
@@ -2,6 +2,7 @@ param (
     [string]$solution = ".\SQLServerDashboard.sln",
     [string]$zipname = "SQLServerDashboard.zip",
 	[string]$compressor = "c:\Program Files\7-Zip\7z.exe",
+	[string]$folder = "SQLServerDashboard",
 	[string]$comment = "New version"
  )
 
@@ -12,10 +13,11 @@ rm ..\Binary\*.* -Force -Recurse
 # build new version
 msbuild /verbosity:minimal $solution
 # Compress bin\debug and move to binary folder
-cmd /c $compressor a -tzip $zipname -r 
+cmd /c $compressor a -tzip $zipname $folder -r 
 cmd /c copy $zipname ..\Binary\ /Y
 
 cd ..
 git add -A *.*
 git commit -a -m $comment
 git push origin master
+cd Source
