2d31168c717a1aa5d34e5d64ca1ce591c624931a
Omar AL Zabir <omaralzabir@gmail.com>
2014-07-18 16:42:54 +0100

    New version

--- Source/gitpush.ps1
+++ Source/gitpush.ps1
@@ -7,21 +7,12 @@ param (
  
 # Clean up binary folder since we will be generating new binaries
 rm ..\Binary\*.* -Force -Recurse
-if (Test-Path "bin" ) { rm -Force bin }
-if (Test-Path "obj" ) { rm -Force obj }
 
 # build new version
 msbuild /verbosity:minimal $solution
 # Compress bin\debug and move to binary folder
-cd bin\debug
-del *vshost*
 cmd /c $compressor a -tzip $zipname *.* 
-cd ../..
-cmd /c copy bin\debug\$zipname ..\Binary\ /Y
-
-# remove bin and obj before comitting to git
-rm -Force bin
-rm -Force obj
+cmd /c copy $zipname ..\Binary\ /Y
 
 cd ..
 git add -A *.*
